dfd6462160436e6b94cec699775ad475a8dda1b7
Yani Moucer <yani9245@gmail.com>
2022-01-31 15:05:28 +0100

    continue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Project 1</title>
 </head>
 <body>
     @include('composant.navbar')
@@ -14,4 +15,5 @@
         @yield('content')
     </div>
 </body>
+    <script src="{{asset('js/app.js')}}"></script>
 </html>
